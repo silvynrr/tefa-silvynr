24a89c50fd3f6e4b3902055b06c2a0012cf199cc
silvynrr <141999356+silvynrr@users.noreply.github.com>
2024-06-05 14:40:10 +0700

    Add files via upload

---
+++ pengunjung/menu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="content">
+    <div class="container-fluid d-flex justify-content-around">
+      <div class="row mx-1 py-5">
+        <div class="col-lg-6 col mb-3">
+          <nuxt-link to="/pengunjung/riwayat">
+            <div class="card bg-pengunjung rounded-5" style="height: 85vh; ">
+              <div class="card-body pt-5">
+                <h2 class="text-center" style="margin-top: 350px;">RIWAYAT KUNJUNGAN</h2>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+
+        <div class="col-lg-6 col">
+          <nuxt-link to="/buku">
+            <div class="card bg-buku rounded-5" style="height: 85vh; padding: 0px 80px;">
+              <div class="card-body pt-5">
+                <h2 class="text-center" style="margin-top: 350px;">CARI BUKU</h2>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.content{
+  background-color: #6AB187;
+  /* height: 100vh; */
+  width: 100%;
+}
+.card {
+  opacity: 85%;
+  color: white;
+  align-items: center;
+  height:250px;
+}
+.card.bg-pengunjung {
+  background-image: url('@/assets/img/bg-home-kunjungan.jpg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+.card.bg-buku {
+  background: url('@/assets/img/bg-home-cari-buku.jpg');
+  background-size: cover;
+}
+</style>
---
+++ pengunjung/riwayat.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">menampilkan 1 dari 1</div>
+        <table class="table table-bordered border-white text-white">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Tanggal</td>
+              <td>Waktu</td>
+              <td>Nama</td>
+              <td>Keanggotaan</td>
+              <td>Kelas</td>
+              <td>Keperluan</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(visitor, i) in visitors" :key="i">
+              <td>{{ i+1 }}.</td>
+              <td>{{ visitor.tanggal }}</td>
+              <td>{{ visitor.waktu }}</td>
+              <td>{{ visitor.nama }}</td>
+              <td>{{ visitor.keanggotaan.nama }}</td>
+              <td>{{ visitor.tingkat}}-{{ visitor.jurusan }}{{ visitor.kelas }}</td>
+              <td>{{ visitor.keperluan.nama }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row float-end">
+        <nuxt-link to="/pengunjung/menu" class="btn btn-dark btn-lg rounded-5 px-5">menu</nuxt-link>
+    </div>
+  </div>
+</template>
+<script setup>
+const supabase = useSupabaseClient()
+
+const visitors = ref([])
+const getPengunjung = async () => {
+  const { data, error } = await supabase.from('pengunjung').select(`*, keanggotaan(*), keperluan(*)`)
+  if(data) visitors.value = data 
+}
+onMounted(() => {
+  getPengunjung()
+})
+</script>
+
+<style scoped>
+.container-fluid {
+  background-image: url('@/assets/img/bg-home-kunjungan.jpg');
+  background-size: cover;
+  height: 100vh;
+  width: 100%;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color:rgb(255, 255, 255);
+  color: white;
+  opacity: 70%;
+}
+.btn{
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background-color: rgb(255, 255, 255);
+  color: black;
+  width: 150px;
+  height: 50px;
+}
+</style>
