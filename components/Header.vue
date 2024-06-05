4ae4d4f02689b6a2dab29e8d08dee581c4050acb
silvynrr <141999356+silvynrr@users.noreply.github.com>
2024-06-05 14:44:23 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row p-3 bg-primary text-white m-0">
+    <div class="col-lg-1">
+      <img src="@/assets/img/LOGO-SMK4.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1>PERPUS DIGITAL</h1>
+      <address>
+        SMKN 4 Tasikmalaya
+        JL.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-primary {
+  background-color: #20948B!important;
+  font-family:cursive;
+  width: 100%;
+  left: 0;
+}
+
+img {
+  width: 100px;
+}
+</style>
